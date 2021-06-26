5a73e416a4c1083aec3ab7dc7ff46f67e144adcf
Yunay <yunay.yuksel@gmail.com>
2021-06-26 11:05:26 +0300

    packages updates

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 const config = {
   ffmpegPath: path.join(__dirname, '../assets/ffmpeg/ffmpeg.exe'),
-  youtubeKey:'AIzaSyCAv1aEG71N3vBEp4rr54ZqhkjjOgjygzE',
+  youtubeKey:'AIzaSyA3oOVLNTftYKMt3LzzI62HsDqAfn9dxTQ',
 }
 
 export default config
